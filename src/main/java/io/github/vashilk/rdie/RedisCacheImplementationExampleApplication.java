a7d7987fa2404f685876d930f89c9f0e716809d3
Neeschal Kissoon <nkissoon@amelco.co.uk>
2023-08-09 01:06:58 +0400

    Initial commit

---
+++ src/main/java/io/github/vashilk/rdie/RedisCacheImplementationExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.vashilk.rdie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheImplementationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCacheImplementationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vashilk/rdie/RedisCacheImplementationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vashilk.rdie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheImplementationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
